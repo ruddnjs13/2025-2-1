7f344adc268a17aba536a3bc980933a4468b10e4
ruddnjs13 <ruddnjs013@gmail.com>
2025-06-04 11:11:34 +0900

    feat : enable towerDragHandler

--- 2025-2-1/Assets/01.Code/Managers/GoldManager.cs
+++ 2025-2-1/Assets/01.Code/Managers/GoldManager.cs
@@ -1,11 +1,14 @@
 using System;
 using Core.GameEvent;
+using TMPro;
 using UnityEngine;
 
 namespace _01.Code.Managers
 {
     public class GoldManager : MonoSingleton<GoldManager>
     {
+        [SerializeField] private TextMeshProUGUI goldText;
+        
         public int Gold { get; private set; }
 
         [SerializeField] private GameEventChannelSO goldChannel;
@@ -24,11 +27,19 @@ namespace _01.Code.Managers
         private void HandleSpendGold(SpendGoldEvent evt)
         {
             Gold -= evt.spendAmount;
+            ChangeGoldUI();
         }
 
         private void HandleGetGold(GetGoldEvent evt)
         {
             Gold += evt.getAmount;
+            ChangeGoldUI();
         }
+
+        private void ChangeGoldUI()
+        {
+            goldText.text = Gold.ToString();
+        }
+        
     }
 }
--- 2025-2-1/Assets/01.Code/Tower/TowerDragHandler.cs
+++ 2025-2-1/Assets/01.Code/Tower/TowerDragHandler.cs
@@ -1,7 +1,12 @@
+using System;
+using System.Xml.Schema;
+using _01.Code.Managers;
 using _01.Code.Tower.Towers;
+using Core.GameEvent;
 using Settings.InputSettings;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace _01.Code.Tower
 {
@@ -11,19 +16,61 @@ namespace _01.Code.Tower
         [SerializeField] private TowerType type;
         
         [SerializeField] private GameObject preview;
+        [SerializeField] private GameObject Visual;
+
+        [SerializeField] private int necessaryGold;
+
+        [SerializeField] private GameEventChannelSO goldChannel;
+
+
+        private Color _enableColor = new Color(1, 1, 1, 1);
+        private Color _disableColor = new Color(1, 1, 1, 0.4f);
         
+        private void Start()
+        {
+            goldChannel.AddListener<GetGoldEvent>(HandleGetGold);
+
+            CheckEnable();
+        }
+
+        private void CheckEnable()
+        {
+            if (GoldManager.Instance.CheckEnoughGold(necessaryGold))
+            {
+                Visual.GetComponent<Image>().color = _enableColor;
+            }
+            else
+            {
+                Visual.GetComponent<Image>().color = _disableColor;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            goldChannel.RemoveListener<GetGoldEvent>(HandleGetGold);
+        }
+
+        private void HandleGetGold(GetGoldEvent evt)
+        {
+            CheckEnable();
+        }
+
+
         public void OnBeginDrag(PointerEventData eventData)
         {
+            if(!GoldManager.Instance.CheckEnoughGold(necessaryGold)) return;
             preview.SetActive(true);
         }
         
         public void OnDrag(PointerEventData eventData)
         {
+            if(!GoldManager.Instance.CheckEnoughGold(necessaryGold)) return;
             preview.transform.position = eventData.position;
         }
 
         public void OnEndDrag(PointerEventData eventData)
         {
+            if(!GoldManager.Instance.CheckEnoughGold(necessaryGold)) return;
             Camera mainCam = Camera.main;
             Ray camRay = mainCam.ScreenPointToRay(inputReader.MousePosition);
 
